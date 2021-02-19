9bd3e805f319e759e01d4f412f00efa0f27f1786
Paul Le Gall <42955270+Creator-360@users.noreply.github.com>
2021-02-20 03:43:54 +0800

    fix electron default size + add images & screenshots

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,27 +1,34 @@
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, Menu, Tray} = require('electron')
 
 
 Menu.setApplicationMenu(null)
-
+//small 330x480
 function createWindow () {
     const main_window = new BrowserWindow({
-      width: 330,
-      height: 480,
+      width: 430,
+      height: 580,
       minWidth: 430,
       minHeight: 580,
       maxWidth: 530,
       maxHeight: 680,
       resizable: true,
       frame: true,
+      maximizable: false,
       alwaysOnTop: false,
+      icon: "./src/images/192logo.png",
       webPreferences: {
         nodeIntegration: true
       }
     })
     main_window.loadFile("./build/index.html")
 }
+const Tray_app = () => {
+    new Tray("./src/images/192logo.png")
+}
 
 app.whenReady().then(() => {
-  createWindow()  
+  //new Tray("./192logo.png")
+  //Tray_app()
+  createWindow()
 })
 //app.whenReady().then(createWindow)
